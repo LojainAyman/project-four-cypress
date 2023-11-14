b2b0f9e2c232f53015750d52d18edc5f66f5eb9a
LojainAyman <lojainalmfleh@gmail.com>
2023-11-14 22:34:56 +0300

    update

---
+++ cypress/e2e/myTest.cy.js
@@ -0,0 +1,13 @@
+///<reference types= "cypress" />
+
+// cy.on("uncaught:exception", (e) =>{
+//   return false;
+// })
+
+
+describe('template spec', () => {
+  it('passes', () => {
+    
+   cy.visit("https://www.almosafer.com/ar")
+  })
+})
